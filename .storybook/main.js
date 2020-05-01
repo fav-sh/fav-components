dfb62b88043862f7ef8c3292297aa0418a5e5bc8
Sunny Golovine <sunny@glvn.co>
2020-05-01 17:01:06 -0400

    Use babel loader instead of TS loader to get module resolution

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,11 +6,10 @@ module.exports = {
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader'),
-        },
-      ],
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [['react-app', { flow: false, typescript: true }]],
+      },
     })
     config.resolve.extensions.push('.ts', '.tsx')
     return config
